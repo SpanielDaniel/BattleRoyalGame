c7ef6944df7545830ac550ab94c73b8e124fab3d
Vitali <kross.vitali@gmail.com>
2020-02-19 20:03:37 +0100

    Fields
    - Cube rename to Field
    - add FieldController with enum FieldState
    - Board Fields change to 14x20
    - add Start Fields, EventCard1/2 Fields
    - add colors to FieldState

---
+++ Assets/Scripts/FieldController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldController : MonoBehaviour
+{
+    private enum FieldState { Normal, Start, Wall, EventCard1, EventCard2}
+
+    [SerializeField] private FieldState _fieldState = FieldState.Normal;
+
+    void Start()
+    {
+        Material Material = transform.GetComponent<Renderer>().material;
+
+        switch (_fieldState) {
+            case FieldState.Normal:
+                Material.color = Color.white;
+                break;
+            case FieldState.Start:
+                Material.color = Color.gray;
+                break;
+            case FieldState.Wall:
+                Material.color = Color.black;
+                break;
+            case FieldState.EventCard1:
+                Material.color = Color.red;
+                break;
+            case FieldState.EventCard2:
+                Material.color = Color.green;
+                break;
+            default:
+                Material.color = Color.white;
+                break;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
