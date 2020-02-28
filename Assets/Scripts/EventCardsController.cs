7193c97aa794032da8594650c589a28302e9abd3
Vitali <kross.vitali@gmail.com>
2020-02-28 15:29:40 +0100

    EventCards and TurnController

---
+++ Assets/Scripts/EventCardsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventCardsController : MonoBehaviour
+{
+    [SerializeField] private GameObject _eventCards;
+    [SerializeField] private GameObject _closeButton;
+    [SerializeField] private GameObject _openButton;
+
+    void Start()
+    {
+        _eventCards.SetActive(false);
+        _closeButton.SetActive(false);
+        _openButton.SetActive(true);
+    }
+
+    public void CloseButton() {
+        _eventCards.SetActive(false);
+        _closeButton.SetActive(false);
+        _openButton.SetActive(true);
+    }
+    public void OpenButton() {
+        _eventCards.SetActive(true);
+        _closeButton.SetActive(true);
+        _openButton.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/TurnController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnController : MonoBehaviour {
+
+    private int _turn = 1;
+    private int _round;
+    private int _playersCount = 2;
+    private string _player1Nickname = "Player1";
+    private string _player2Nickname = "Player2";
+
+    private Text _roundText;
+    private Text _turnText;
+    private Text _timerText;
+
+    private float _maxTimer = 30f;
+    private float _currentTimer;
+
+    private void Start() {
+        _roundText = transform.Find("Round").GetComponent<Text>();
+        _turnText = transform.Find("Turn").GetComponent<Text>();
+        _timerText = transform.Find("Time").GetComponent<Text>();
+
+        _roundText.text = "Round: 1";
+        _turnText.text = "Turn: " + _player1Nickname;
+        _timerText.text = _maxTimer.ToString();
+        _timerText.color = Color.white;
+        _currentTimer = _maxTimer;
+    }
+    private void Update() {
+        if (_currentTimer > 0) {
+            _currentTimer -= 1 * Time.deltaTime;
+            _timerText.text = Math.Round(_currentTimer).ToString();
+
+            if (_currentTimer < 5.5) {
+                _timerText.color = Color.red;
+            }
+        }
+        else {
+            EndTurn();
+        }
+    }
+    public void EndTurn() {
+        TurnUpdate();
+        RoundUpdate();
+        TimerUpdate();
+    }
+    private void TurnUpdate() {
+        _turn++;
+        _turnText.text = "Turn: " + ((_turn % 2 == 1) ? _player1Nickname : _player2Nickname);
+    }
+    private void RoundUpdate() {
+        _round = (int) Math.Ceiling((float)_turn / _playersCount);
+        _roundText.text = "Round: " + _round;
+    }
+    private void TimerUpdate() {
+        _currentTimer = _maxTimer;
+        _timerText.text = _currentTimer.ToString();
+        _timerText.color = Color.white;
+    }
+}
